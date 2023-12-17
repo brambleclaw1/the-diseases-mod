01150e7465bff8883d85d938f73d8afc294e4f37
brambleclaw1 <unconfigured@null.spigotmc.org>
2023-12-16 18:26:19 -0700

    warning: it broken

--- src/main/java/net/mcreator/thediseasesmod/procedures/TheBlackDeathOnEffectActiveTickProcedure.java
+++ src/main/java/net/mcreator/thediseasesmod/procedures/TheBlackDeathOnEffectActiveTickProcedure.java
@@ -2,6 +2,8 @@ package net.mcreator.thediseasesmod.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.Mth;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.CommandSource;
@@ -71,6 +73,18 @@ public class TheBlackDeathOnEffectActiveTickProcedure {
 													"attribute @s minecraft:generic.max_health base set 2");
 										}
 									}
+									TheDiseasesModMod.queueServerWork(20, () -> {
+										if (Mth.nextDouble(RandomSource.create(), 1, 5) == 1) {
+											{
+												Entity _ent = entity;
+												if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+													_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(),
+															_ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4, _ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent),
+															"attribute @s minecraft:generic.max_health base set 2");
+												}
+											}
+										}
+									});
 								});
 							});
 						});
